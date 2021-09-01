0d4bc66fbb64c66ffa4c0cee45848980f33424fb
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2021-09-01 11:16:19 +0100

    Add flexbox to footer, hover color, change about me

--- src/header-footer.js
+++ src/header-footer.js
@@ -1,7 +1,7 @@
 const template = document.createElement("template");
 template.innerHTML = `
-  <style>
-    .template-header {
+  <!--<style>
+    #template-header {
       font-size: 10px;
       color: beige;
       text-decoration: none;
@@ -10,21 +10,41 @@ template.innerHTML = `
       left: 1%;
     }
 
-    footer {
+    #template-footer {
+      display: flex;
+      justify-content: space-between;
+      text-decoration: none;
+      width: 500px;
+      height: 500px;
       position: absolute;
-      bottom: 2%;
       left: 50%;
-      transform: translate(-50%, -50%);
+      top: 50%;
     }
+
+    #contact {
+      background-color: blue;
+      width: 50%;
+      height: 50%;
+    }
+
+    #source-code {
+      background-color: red;
+      width: 50%;
+      height: 50%;
+    }
+
   </style>
   <header>
-    <a class="template-header" href="index.html">
+    <a id="template-header" href="index.html">
       <h1>diogo gaspar</h1>
     </a>
   </header>
   <footer>
-    <p>contact me on discord - gaspa#4057</p>
-  </footer>
+    <div id="template-footer">
+      <p id="contact" >contact me on discord - gaspa#4057</p>
+      <a id="source-code" href="https://github.com/randomicecube/personal-webpage">source code @ github</a>
+    </div>
+  </footer> -->
 `;
 
 document.body.appendChild(template.content);
