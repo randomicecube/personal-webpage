c55ab0f851bb591fcd12db1bd68a65e520e60f3b
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2021-09-01 11:21:48 +0100

    Change header and footer to common file

--- src/header-footer.js
+++ src/header-footer.js
@@ -1,38 +1,60 @@
+// this file's purpose is to have a header and footer shared by all the website's html pages
+
 const template = document.createElement("template");
 template.innerHTML = `
-  <!--<style>
+  <style>
+    /* header classes and ids */
+  
     #template-header {
       font-size: 10px;
       color: beige;
       text-decoration: none;
       position: absolute;
-      top: 1%;
       left: 1%;
     }
-
-    #template-footer {
+    
+    #template-header:hover {
+      color: white;
+      transition: all 0.3s ease;
+    }
+    
+    /* footer classes and ids */
+    
+    .template-footer {
+      font-size: 14px;
       display: flex;
       justify-content: space-between;
-      text-decoration: none;
-      width: 500px;
-      height: 500px;
+      flex-direction: column;
+      height: 100px;
+      width: 300px;
       position: absolute;
       left: 50%;
-      top: 50%;
+      top: 92.5%;
+      transform: translate(-50%, -50%);
     }
-
-    #contact {
-      background-color: blue;
-      width: 50%;
-      height: 50%;
+    
+    #footer-box-1 {
+      width: 100%;
+      height: 100%;
+      text-align: center;
     }
-
+    
+    #footer-box-2 {
+      width: 100%;
+      height: 100%;
+      text-align: center;
+    }
+    
     #source-code {
-      background-color: red;
-      width: 50%;
-      height: 50%;
+      color: beige;
+      text-decoration: none;
     }
-
+    
+    #source-code:hover {
+      color: cornflowerblue;
+      transition: all 0.3s ease;
+    }
+  
   </style>
   <header>
     <a id="template-header" href="index.html">
@@ -40,11 +62,21 @@ template.innerHTML = `
     </a>
   </header>
   <footer>
-    <div id="template-footer">
-      <p id="contact" >contact me on discord - gaspa#4057</p>
-      <a id="source-code" href="https://github.com/randomicecube/personal-webpage">source code @ github</a>
+    <div class="template-footer">
+      <div id="footer-box-1">
+        <p>contact me on discord - gaspa#4057</p>
+      </div>
+      <div id="footer-box-2">
+        <a
+          id="source-code"
+          href="https://github.com/randomicecube/personal-webpage"
+          target="_blank"
+        >
+          <p>source code @ github</p>
+        </a>
+      </div>
     </div>
-  </footer> -->
+  </footer>
 `;
 
 document.body.appendChild(template.content);
